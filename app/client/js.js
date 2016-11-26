c7a15384c1450670d4ebbc9cd6d0fd2731cff441
littleguga <fed777os@gmail.com>
2016-11-26 12:01:31 +0200

    add draft of api wrap

--- app/client/js.js
+++ app/client/js.js
@@ -30,23 +30,88 @@ function registerSocket() {
 
             /*
              id
-             itemId
+             ean
              status
-            */
+             */
             console.log(data.order);
+
+            api.getItemInfo(data.order.ean);
         });
     });
 }
 
 
-function displayNewOrder(order){
-    
-}
-
-function moveOrderToPacked(){
+function displayNewOrder(order) {
 
 }
 
-function moveOrderToDone(){
+function moveOrderToPacked() {
 
+}
+
+function moveOrderToDone() {
+
+}
+
+var api = {
+    key: 'N1yF3gF4bE6vT1eL3aJ1jT2hH6wI0uV4iX8eJ3xK7tO0lA2jN8',
+    url: 'https://api.eu.apiconnect.ibmcloud.com/kesko-dev-rauta-api/qa/products/',
+    language: 'fi',
+    ibmid: '4bc37a99-049e-4a03-8c35-270810e7e851',
+    getItemInfo: function (ean) {
+        var headers = [
+            ['X-IBM-Client-Id', api.ibmid],
+            ['X-IBM-Client-Secret', api.key],
+            ['accept-language', api.language],
+            ['content-type', 'application/json'],
+            ['accept', 'application/json']
+        ];
+
+        getJson(api.url + ean, headers, function (data) {
+            return {
+                name: data.name,
+                desc: data.description,
+                ean: ean,
+                images: data.images
+            };
+        });
+    }
+};
+
+function getImageUrl(images) {
+    var height = 400,
+        width = 400;
+    for (var i = 0; i < images.length; i++) {
+        if (images[i].type.toLowerCase() === 'product') {
+            return images[i].url + '?h=' + height + '&w=' + width + '&fit=fill';
+        }
+    }
+}
+
+function getJson(url, headers, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+
+    //set headers
+    headers.forEach(function (h) {
+        request.setRequestHeader(h[0], h[1]);
+    });
+
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            // Success!
+            var data = JSON.parse(this.response);
+            callback(data);
+        } else {
+            // We reached our target server, but it returned an error
+            callback(false, 'error happen', this.status);
+        }
+    };
+
+    request.onerror = function (err) {
+        // There was a connection error of some sort
+        callback(false, 'error happen');
+    };
+
+    request.send();
 }
